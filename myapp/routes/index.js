786ad123dd70c3b81fec41ed57e4d6686a54595f
Your Name <lovelyloralee@gmail.com>
2018-11-15 03:55:12 +0000

    functionality and style last tweeks

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,21 +1,79 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-//var fs = require('fs');
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var dbUrl = 'mongodb://localhost:27017/';
+var collection;
 
-router.get('/', function(req, res, next) {
-  res.sendFile('index.html', { root: 'public' });
+MongoClient.connect(dbUrl, { useNewUrlParser: true }, function(err, client) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  }
+  else {
+    console.log('Connection established to', dbUrl);
+    var db = client.db('memory');
+    db.createCollection('leaderboard', function(err, result) {
+      if (err) {
+        console.log('Creating collections failed', err);
+      }
+      else {
+        console.log("Hooray! Collection worked");
+        collection = result;
+        collection.stats(function(err, stats) {
+          if (err) {
+            console.log('Stats messed up', err);
+          }
+          else {
+            if (leaders.length == 0) {
+              console.log("Zero leaders yet");
+            }
+          }
+        });
+      }
+    });
+  }
 });
 
-router.get('/json', function(req, res, next) {
-  var url = "https://api.sunrise-sunset.org/";
-  console.log("query ", req.query);
-  url += "json?lat=" + req.query["lat"];
-  url += "&lng=";
-  url += req.query["lng"];
-  url += "&date=today";
-  console.log(url.toString());
-  request(url).pipe(res);
+router.get('/leaders', function(req, res, next) {
+  console.log("Getting Leaders");
+  collection.find().toArray(function(err, result) {
+    if (err) {
+      console.log('Bad', err);
+    }
+    else {
+      console.log("All good");
+      res.send(result);
+    }
+  });
 });
+
+router.post('/leaders', function(req, res) {
+  console.log("Leaders Post");
+  console.log(req.body);
+  collection.insertOne(req.body, function(err, result) {
+    if (err) {
+      console.log('Bad', err);
+    }
+    else {
+      console.log("All good");
+      res.end('{"success" : "Updated Successfully", "status" : 200}');
+    }
+  })
+});
+
+router.delete('/leaders', function(req, res, next) {
+    console.log("delete route");
+    collection.deleteMany({}, function(err, result) {
+        if (err) return console.error(err);
+        else {
+            console.log("Deleting all");
+            console.log("Leftovers: ", result);
+            res.sendStatus(200);
+        }
+    });
+});
+
+var leaders = [];
 
 module.exports = router;
